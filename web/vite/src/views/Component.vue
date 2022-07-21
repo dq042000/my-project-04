ab8015ec9daa270a0ea7283344aa438994e0f872
祁威德 <dq042000@gmail.com>
2022-07-21 16:29:48 +0800

    修改組件

--- web/vite/src/views/Component.vue
+++ web/vite/src/views/Component.vue
@@ -1,25 +1,31 @@
+<!-- ---父組件 傳 子組件----------------------------------- -->
 <!-- <template>
     <div>
         <List :msg="msg"></List>
-        <List2 @fn="changeHome"></List2>
     </div>
 </template>
 
 <script setup>
 import List from "../components/List.vue";
-import List2 from "../components/List2.vue";
-
-// 父 傳 子
 let msg = ref("這是父傳過來的資料");
+</script> -->
 
-// 子 傳 父
+<!-- ---子組件 傳 父組件----------------------------------- -->
+<!-- <template>
+    <div>
+        <List2 @fn="changeHome"></List2>
+    </div>
+</template>
+
+<script setup>
+import List2 from "../components/List2.vue";
 const changeHome = (n) => {
-    this.str = n.value;
+    console.log(n.value);
 };
 </script> -->
 
 <!-- ---父子組件雙向資料----------------------------------- -->
-<template>
+<!-- <template>
     <div>
         <List3 v-model:num="num"></List3>
     </div>
@@ -28,4 +34,6 @@ const changeHome = (n) => {
 <script setup>
 import List3 from "../components/List3.vue";
 let num = ref(1);
-</script>
+</script> -->
+
+<!-- ---兄弟組件之間的傳值----------------------------------- -->
