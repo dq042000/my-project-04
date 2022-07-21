731fdb4a656d38fde06acb9570a0d8b0f1b83fbf
祁威德 <dq042000@gmail.com>
2022-07-21 15:14:38 +0800

    ./web/vite/src/router/index.ts 增加筆記

--- web/vite/src/router/index.ts
+++ web/vite/src/router/index.ts
@@ -53,11 +53,28 @@ const router = createRouter({
     routes: constantRouterMap as RouteRecordRaw[],
 });
 
-// 路由守衛
-router.beforeEach(() => {
+// 開始進入新路由之前 (全域)
+// https://book.vue.tw/CH4/4-4-navigation-guards.html#beforeeach-%E5%85%A8%E5%9F%9F
+//
+// 我們可以直接在 router.beforeEach 註冊對應的 callback
+// 這樣當「每一個」路由要進入之前，都會先經過這裡
+router.beforeEach((to, from) => {
     NProgress.start();
+    console.log("to:", to);
+    console.log("from:", from);
 });
 
+// 當路由跳轉結束後 (全域)
+// https://book.vue.tw/CH4/4-4-navigation-guards.html#aftereach-%E5%85%A8%E5%9F%9F
+//
+// 在路由跳轉結束後才會觸發。
+// 它的 callback 函式只有 to 、 from 不會有 next 參數
+// ，因此不會影響路由的跳轉。
+//
+// 但是多了一個 failure 參數用來表示路由跳轉失敗。
+//
+// 所以我們可以使用 router.afterEach 來搭配像是 GA 追蹤一類的工具，
+// 來記錄使用者的瀏覽紀錄。
 router.afterEach(() => {
     NProgress.done();
 });
