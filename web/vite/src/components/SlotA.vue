fe4e928a899113b0870a16caa69a759069c21b7a
祁威德 <dq042000@gmail.com>
2022-07-22 10:44:32 +0800

    Slot

--- web/vite/src/components/SlotA.vue
+++ web/vite/src/components/SlotA.vue
@@ -1,8 +1,40 @@
 <template>
     <div>
-        <h1>A組件</h1>
-        <button @click="btn">按鈕</button>
+        <!-- 具名插槽 -->
+        <h1>子：A組件</h1>
+        <header>
+            <div>頭部</div>
+            <slot name="xxx"></slot>
+            <slot name="zzz"></slot>
+        </header>
+        <hr />
+        <footer>
+            <div>底部</div>
+            <slot name="yyy"></slot>
+        </footer>
+        <hr />
+
+        <!-- 作用域插槽 -->
+        <section>
+            <div>中間</div>
+            <div v-for="item in list" :key="item.id">
+                <slot :data="item"></slot>
+            </div>
+        </section>
+        <hr />
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+let list = ref([
+    { id: 1, name: "李一", age: 26 },
+    { id: 2, name: "李二", age: 31 },
+    { id: 3, name: "李三", age: 25 },
+    { id: 4, name: "李四", age: 22 },
+    { id: 5, name: "李五", age: 41 },
+    { id: 6, name: "李六", age: 46 },
+    { id: 7, name: "李七", age: 36 },
+    { id: 8, name: "李八", age: 38 },
+    { id: 9, name: "李九", age: 29 },
+]);
+</script>
--- web/vite/src/views/Slot.vue
+++ web/vite/src/views/Slot.vue
@@ -1,11 +1,29 @@
 <template>
     <div>
-        <SlotA></SlotA>
+        <SlotA>
+            <!-- 具名插槽 -->
+            <!-- # 是 v-slot 的簡寫 -->
+            <!-- #xxx 效果等同 v-slot:xxx -->
+            <template #xxx> 這是xxx資料 </template>
+            <template v-slot:yyy> 這是yyy資料 </template>
+
+            <!-- 動態插槽 -->
+            <template #[zzz]> 這是zzz資料 </template>
+
+            <!-- 作用域插槽 -->
+            <!-- #default="{data}" 效果等同 v-slot="{ data }" -->
+            <template v-slot="{ data }"
+                >{{ data.name }} ---> {{ data.age }}</template
+            >
+        </SlotA>
     </div>
 </template>
 
 <script setup>
 import SlotA from "../components/SlotA.vue";
+
+// 動態插槽
+let zzz = ref("zzz");
 </script>
 
 <style></style>
